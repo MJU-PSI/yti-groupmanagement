141e2ff98e5c2cc2c69e403fa80089b2f8df76dd
Tadej Šivak <tadej.sivak@gora.si>
2023-04-25 14:23:58 +0200

    update base href from ANGULAR_BASE_HREF

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 set -e
 
+# Extract from JAR file
+jar -xf yti-groupmanagement.jar static/index.html
 jar -xf yti-groupmanagement.jar static/configuration/configuration.json
 jar -xf yti-groupmanagement.jar static/configuration/configuration.template.json
 
 # It is possible to overwrite configuration via environment variables.
 envsubst < ./static/configuration/configuration.template.json > ./static/configuration/configuration.json
 
+# Replace base href in index.html
+if [[ $ANGULAR_BASE_HREF != */ ]] # * is used for pattern matching
+then
+  ANGULAR_BASE_HREF="${ANGULAR_BASE_HREF}/";
+  sed -i 's#<base href="./">#<base href="'"${ANGULAR_BASE_HREF}"'">#' ./static/index.html
+fi
+
 # Set read permissions for everyone on the file
+chmod +r ./static/index.html
 chmod +r ./static/configuration/configuration.json
 
+# Update JAR file
+jar -uf yti-groupmanagement.jar static/index.html
 jar -uf yti-groupmanagement.jar static/configuration/configuration.json
 
 rm -rf ./static
